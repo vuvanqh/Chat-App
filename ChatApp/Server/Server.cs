0bcc0300e6f0a7dd024ac88c5c82d4be4fe23585
BanhBaoz <q.vuvan05@gmail.com>
2025-02-24 18:38:32 +0100

    Client1

---
+++ ChatApp/Server/Server.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client.NewFolder;
+
+public class NetworkException : Exception
+{
+    public NetworkException(string? message) : base(message) { }
+}
+
+
+
+public class Server
+{
+    TcpClient client;
+
+    public Server() 
+    {
+        client = new TcpClient();
+    }
+
+    public IPAddress? GetServer()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip;
+            }
+        }
+        return null;
+    }
+
+    public void ConnectToServer()
+    {
+        if (!client.Connected)
+        {
+            try
+            {
+                var ip = GetServer();
+                if (ip == null)
+                {
+                    throw new NetworkException("can't find the LAN IP");
+                }
+                client.Connect(ip, 3145);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Invalid lan ip retreival logic:\n{e.Message}");
+                client.Connect("127.0.0.1", 3145);
+            }
+        }
+    }
+}
+
---
+++ Server/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics;
+
+namespace Server;
+public class Program
+{
+    class UpCounter
+    {
+        public void CountUp(int count)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("\n Count-Up starts");
+            for (int i =1; i <= count; i++)
+            {
+                Console.Write($"i = {i}, ");
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine($"\nCount-Up ends: {stopwatch.ElapsedMilliseconds}");
+        }
+        public void CountDown(int count)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("\n Count-Down starts");
+            for (int i = count; i>0; i--)
+            {
+                Console.Write($"j = {i}, ");
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine($"\nCount-down ends: {stopwatch.ElapsedMilliseconds}");
+        }
+    }
+    static public void Th()
+    {
+        UpCounter up = new UpCounter();
+        CountdownEvent count = new CountdownEvent(2);
+
+        Thread t1 = new Thread(() =>
+        {
+            up.CountDown(20);
+            count.Signal();
+        });
+        Thread t2 = new Thread(() =>
+        {
+            up.CountUp(20);
+            count.Signal();
+        });
+        t1.Start();
+        t2.Start();
+
+        count.Wait();
+    }
+    static public void Ts()
+    {
+        UpCounter up = new UpCounter();
+        CountdownEvent count = new CountdownEvent(2);
+
+        Task task1 = Task.Run(() => { up.CountDown(20); count.Signal(); });
+        Task task2 = Task.Run(() => { up.CountUp(20); count.Signal(); });
+
+        count.Wait();
+    }
+    static void Main()
+    {
+        Stopwatch stopwatch = new Stopwatch();
+
+        stopwatch.Start();
+        Ts();
+        stopwatch.Stop();
+        Console.WriteLine($"Tasks: {stopwatch.ElapsedMilliseconds}");
+        stopwatch.Restart();
+        Th();
+        stopwatch.Stop();
+        Console.WriteLine($"Threads: {stopwatch.ElapsedMilliseconds}");
+        Console.WriteLine("\nCounting ended");
+    }
+}
